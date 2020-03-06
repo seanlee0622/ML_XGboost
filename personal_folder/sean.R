651c81c8824669efcd6bbcaba982c58a2e26afcc
Sean Lee <49972734+seanlee0622@users.noreply.github.com>
2020-03-05 22:06:56 -0700

    .

--- personal_folder/sean.R
+++ personal_folder/sean.R
@@ -1,1 +1,2 @@
+# Load the packages
 pacman::p_load(data.table, janitor, tidyverse, DataExplorer, naniar, skimr, plotly, corrplot, DT, gridExtra, ggrepel, stringi, lubridate, caret, xgboost, rattle, Boruta)
